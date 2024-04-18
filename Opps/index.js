ccc31c42840277efdc0f81a66a9fad6f02223314
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-18 17:00:41 +0530

    Opps in js

---
+++ Opps/index.js
@@ -0,0 +1,44 @@
+const stud1={
+    name:"shubham",
+    marks:98,
+    collage:"sinhgad collage of science",
+    class:"fy"
+}
+const stud2={
+    name:"pavan",
+    marks:99,
+    collage:"sinhgad collage of comeerse",
+    class:"sy"
+}
+const stud3={
+    name:"ashish",
+    marks:100,
+    collage:"jedhe collage",
+    class:"sy"
+}
+
+const stud4={
+    name:"omkar",
+    marks:100,
+    collage:"sinhgad collage of science",
+    class:"fy"
+}
+
+const stud5={
+    name:"suraj",
+    marks:100,
+    collage:"sinhgad collage of science",
+    class:"fy"
+}
+const stud6={
+    name:"sayali",
+    marks:100,
+    collage:"sinhgad collage of science",
+    class:"fy"
+}
+const stud7={
+    name:"prasad",
+    marks:100,
+    collage:"sinhgad collage of science",
+    class:"fy"
+}
